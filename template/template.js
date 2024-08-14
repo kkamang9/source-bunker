99ade4de33b5a7028e52c3a58264d08831689b81
kkamang9 <kkamang797@gmail.com>
2024-08-14 17:48:01 +0900

    240814

--- template/template.js
+++ template/template.js
@@ -1,53 +1,28 @@
 $(function(){
     // $(function(){}); = $(document).ready(function(){ }); = document.onload()
-    var $copyBox = $('.copy_box');
-    $copyBox.each(function(){
+    
+    // copy button script
+    $('.copy_box').each(function(){
         var $copyButton = $(this).find('.copy'),
-            $copyTextarea = $(this).find('textarea.copy_text');
+            $copyTextarea = $(this).find('textarea.copy_text').text();
         $copyButton.on('click',function(){
-            $copyTextarea.select();
-            var copyClipboard = document.execCommand('copy');
+            navigator.clipboard.writeText($copyTextarea).then();
         });
     });
-});
-
-
-
-
-/*템플릿 코드 복사 코드
     
-    @name code-copy
-    * @author Kwon Oh Hwan
-    * @since 2020-09-16
+    // tab menu
+    var $tabWrap = $('.tab_wrap');
+    $tabWrap.each(function(){
+        var $this = $(this),
+            $tabMenuItem = $this.find('.tab_menu .tab_list .tab_item'),
+            $tabBtn = $tabMenuItem.find('.btn');
+        $tabBtn.on('click',function(){
+            var $thisBtn = $(this),
+                $tabIndex = $thisBtn.parent().index(),
+                $tabContentItem = $thisBtn.closest($tabWrap).find('.tab_contents .content_item');
+            $thisBtn.attr('title','선택됨').parent().addClass('active').siblings().removeClass('active').find($tabBtn).removeAttr('title');
+            $tabContentItem.removeClass('active').eq($tabIndex).addClass('active');
+        });
+    });
     
-    1. 복사할 코드에 attribute로 copy-attr='yes' 기입
-    2. <textarea id="ClipBoard" style="display:none"></textarea> html 파일에 넣기
-    3. <div class="copy_clone" style="display:none;"></div> html 파일에 넣기
-    
-    */
-
-$("[copy-attr='yes']").on('click', function() {
-    var findHtml = $(this).clone();
-    
-    
-    $('.copy_clone').append(findHtml);
-    $('.copy_clone *').removeAttr('copy-attr style');
-    
-    var copyHtml = $('.copy_clone').html();
-    
-    
-    $('#ClipBoard').css('display', 'block').val(copyHtml).select();
-    
-    var succeed;
-    try {
-        succeed = document.execCommand("copy");
-        $('#ClipBoard').css('display', 'none');
-        $('.copy_clone').html('');
-    } catch(e) {
-        succeed = false;
-    }
-    if(succeed) {
-        alert('복사 되었습니다');
-    }
-    return false;
 });
