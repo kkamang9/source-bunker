7a17082dedf75f5959783ab5261ae69b2739c311
kkamang9 <kkamang797@gmail.com>
2024-08-22 10:30:16 +0900

    template layout

--- template/js/template.js
+++ template/js/template.js
@@ -14,14 +14,27 @@ $(function(){
     var $tabWrap = $('.tab_wrap');
     $tabWrap.each(function(){
         var $this = $(this),
-            $tabMenuItem = $this.find('.tab_menu .tab_list .tab_item'),
-            $tabBtn = $tabMenuItem.find('.btn');
+            $tabOpen = $this.find('.tab_menu > .tab_open'),
+            $tabBtn = $this.find('.tab_menu > .tab_list > .tab_item > button.btn');
+        $tabOpen.off().on('click',function(){
+            var $thisOpen = $(this);
+            if($thisOpen.is('.active') === true){
+                $thisOpen.removeClass('active').attr('title','탭메뉴 열기').next().stop().slideUp();
+            }else if($thisOpen.is('.active') === false){
+                $thisOpen.addClass('active').attr('title','탭메뉴 닫기').next().stop().slideDown();
+            }
+        });
         $tabBtn.on('click',function(){
             var $thisBtn = $(this),
                 $tabIndex = $thisBtn.parent().index(),
-                $tabContentItem = $thisBtn.closest($tabWrap).find('.tab_contents .content_item');
-            $thisBtn.attr('title','선택됨').parent().addClass('active').siblings().removeClass('active').find($tabBtn).removeAttr('title');
-            $tabContentItem.removeClass('active').eq($tabIndex).addClass('active');
+                $thisText = $thisBtn.find('span > em').text(),
+                $prevOpen = $thisBtn.closest('.tab_list').prev();
+            $thisBtn.attr('title','선택됨').parent().addClass('active').siblings().removeClass('active').children('.btn').removeAttr('title');
+            $thisBtn.closest('.tab_menu').siblings('.tab_content').children('.content_item').removeClass('active').eq($tabIndex).addClass('active');
+            if($prevOpen.is('.active')){
+                $prevOpen.removeClass('active').attr('title','탭메뉴 열기').next().stop().slideUp();
+                $prevOpen.find('span > em').text($thisText);
+            }
         });
     });
     // tab menu - E
