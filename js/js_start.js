d997ec99c713066434fa2e6d76fb10792a3aec7e
kkamang9 <kkamang797@gmail.com>
2024-11-22 17:03:51 +0900

    241122

---
+++ js/js_start.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // jQuery 시작 기본문 - html 문서 로딩이 끝나면 실행
+});
+
+$(function() {
+    // jQuery 시작 축약문 - html 문서 로딩이 끝나면 실행
+});
+
+(function ($) {
+    // jQuery 외의 라이브러리와의 충돌을 막기위한 한겹의 보호막
+    'use strict';
+    // 엄격 모드 - 디버깅 편의를 위해 사용(sloppy 모드에서 조용히 실패하거나 문제의 요지가 있는 코드에 오류가 뜨게 함)
+})(jQuery);
+
+document.addEventListener("DOMContentLoaded", function(){
+    // 순수 javascript로 ready()를 대체하는 코드.
+});
