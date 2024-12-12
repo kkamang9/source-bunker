ba8f945aca481700483375dd3800f11ba39f2be1
kkamang9 <kkamang797@gmail.com>
2024-12-12 17:50:20 +0900

    template layout

--- template/js/template.js
+++ template/js/template.js
@@ -11,6 +11,24 @@
         });
         // copy button script - E
         
+        // 반응형 테이블
+        $('table.table.responsive').not($('.prettyprint').children()).each(function () {
+            var RowSpanExist = $(this).find('td, th').is('[rowspan]'),
+                TheadExist = $(this).find('thead').length;
+            if ((RowSpanExist == false) && (TheadExist != 0)) {//rowspan이 없을 경우만 실행 (rowspan이 있으면 지원불가)
+                $(this).children('tbody').children('tr').find('th, td').each(function () {
+                    var ThisIndex = $(this).index(),
+                        TheadText = $(this).parents('tbody').siblings('thead').find('th').eq(ThisIndex).text();
+                    $(this).attr('data-content', TheadText);
+                });
+                $(this).children('tfoot').children('tr').find('th, td').each(function () {
+                    var ThisIndex = $(this).index(),
+                        TheadText = $(this).parents('tfoot').siblings('thead').find('th').eq(ThisIndex).text();
+                    $(this).attr('data-content', TheadText);
+                });
+            }
+        });
+        
         // tab menu - S
         $('.tab_wrap').each(function(){
             var $this = $(this),
